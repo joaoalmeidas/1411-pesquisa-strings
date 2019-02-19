0f2413822389fdbb731fc76de884b991f8cb6db3
joao <joao182cx@gmail.com>
2019-02-19 11:29:13 -0300

    Criação do código que lê a frase e o caractere.

---
+++ src/Pesquisa.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pesquisa {
+
+	public static void main(String[] args) {
+		
+		String frase;
+		String caractere;
+		
+		Scanner input = new Scanner(System.in);	
+		
+		System.out.println("Insira uma frase:");
+		frase = input.nextLine();
+		
+		System.out.println("Insira um caractere para pesquisar na frase:");
+		caractere = input.next();
+		
+		for(int i = 0; i < frase.length(); i++) {
+			
+			
+			
+		}
+		
+	}
+
+}
